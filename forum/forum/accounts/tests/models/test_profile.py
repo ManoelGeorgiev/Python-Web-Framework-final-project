d6ca56791d44723bc39715501b9ef5465552546f
ManoelGeorgiev <manoel.y.georgiev@gmail.com>
2022-04-13 22:26:37 +0300

    changed profile model tests

--- forum/forum/accounts/tests/models/test_profile.py
+++ forum/forum/accounts/tests/models/test_profile.py
@@ -4,25 +4,17 @@ from datetime import date
 from io import BytesIO
 
 from PIL import Image
-from django.contrib.staticfiles import finders
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.files import File
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
 from forum import settings
-from forum.accounts.models import Profile, ForumUser
+from forum.accounts.models import Profile
 
+UserModel = get_user_model()
 
 class ProfileTests(TestCase):
-
-    @staticmethod
-    def get_image_file(name='test.png', ext='png', size=(50, 50), color=(256, 0, 0)):
-        file_obj = BytesIO()
-        image = Image.new("RGB", size=size, color=color)
-        image.save(file_obj, ext)
-        file_obj.seek(0)
-        return File(file_obj, name=name)
 
     VALID_USER_DATA = {
         'username': 'manoel',
@@ -35,21 +27,21 @@ class ProfileTests(TestCase):
         'date_of_birth': date(1993, 9, 29),
         'bio': 'this is a bio',
         'email': 'test@email.com',
+        'picture': os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, 'test.png')
     }
 
     def test_profile_create__when_first_name_contains_only_letters__expect_success(self):
-        user = ForumUser.objects.create_user(**self.VALID_USER_DATA)
+        user = UserModel.objects.create_user(**self.VALID_USER_DATA)
 
         profile = Profile(
             **self.VALID_PROFILE_DATA,
             user=user,
-            picture=self.get_image_file()
         )
         profile.save()
         self.assertIsNotNone(profile.pk)
 
     def test_profile_create__when_first_name_contains_a_digit__expect_fail(self):
-        user = ForumUser.objects.create_user(**self.VALID_USER_DATA)
+        user = UserModel.objects.create_user(**self.VALID_USER_DATA)
         first_name = '1Manoel1'
         profile = Profile(
             first_name=first_name,
@@ -58,7 +50,7 @@ class ProfileTests(TestCase):
             bio=self.VALID_PROFILE_DATA['bio'],
             email=self.VALID_PROFILE_DATA['email'],
             user=user,
-            picture=self.get_image_file()
+            picture=os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, 'test.png')
         )
 
         with self.assertRaises(ValidationError) as context:
@@ -68,7 +60,7 @@ class ProfileTests(TestCase):
         self.assertIsNotNone(context.exception)
 
     def test_profile_create__when_first_name_contains_a_dollar_sign__expect_fail(self):
-        user = ForumUser.objects.create_user(**self.VALID_USER_DATA)
+        user = UserModel.objects.create_user(**self.VALID_USER_DATA)
         first_name = 'Mano&el'
         profile = Profile(
             first_name=first_name,
@@ -77,7 +69,7 @@ class ProfileTests(TestCase):
             bio=self.VALID_PROFILE_DATA['bio'],
             email=self.VALID_PROFILE_DATA['email'],
             user=user,
-            picture=self.get_image_file()
+            picture=os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, 'test.png')
         )
 
         with self.assertRaises(ValidationError) as context:
@@ -87,7 +79,7 @@ class ProfileTests(TestCase):
         self.assertIsNotNone(context.exception)
 
     def test_profile_create__when_first_name_contains_a_space__expect_fail(self):
-        user = ForumUser.objects.create_user(**self.VALID_USER_DATA)
+        user = UserModel.objects.create_user(**self.VALID_USER_DATA)
         first_name = 'Mano el'
         profile = Profile(
             first_name=first_name,
@@ -96,7 +88,7 @@ class ProfileTests(TestCase):
             bio=self.VALID_PROFILE_DATA['bio'],
             email=self.VALID_PROFILE_DATA['email'],
             user=user,
-            picture=self.get_image_file()
+            picture=os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, 'test.png')
         )
 
         with self.assertRaises(ValidationError) as context:
@@ -111,7 +103,7 @@ class ProfileTests(TestCase):
         self.assertEqual(expected_fullname, profile.full_name)
 
     def test_profile_create__when_picture_is_under_5_mb__expect_success(self):
-        user = ForumUser.objects.create_user(**self.VALID_USER_DATA)
+        user = UserModel.objects.create_user(**self.VALID_USER_DATA)
         first_name = 'Manoel'
         profile = Profile(
             **self.VALID_PROFILE_DATA,
@@ -123,14 +115,7 @@ class ProfileTests(TestCase):
         self.assertIsNotNone(profile.pk)
 
     def test_profile_create__when_picture_is_more_than_5_mb__expect_fail(self):
-        def get_image_file(name='test.png', ext='png', size=(20000, 15000), color=(256, 0, 0)):
-            file_obj = BytesIO()
-            image = Image.new("RGB", size=size, color=color)
-            image.save(file_obj, ext)
-            file_obj.seek(0)
-            return File(file_obj, name=name)
-
-        user = ForumUser.objects.create_user(**self.VALID_USER_DATA)
+        user = UserModel.objects.create_user(**self.VALID_USER_DATA)
         profile = Profile(
             first_name=self.VALID_PROFILE_DATA['first_name'],
             last_name=self.VALID_PROFILE_DATA['last_name'],
